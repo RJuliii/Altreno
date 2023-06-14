49b37195e3b525fb54fe3d3b83100b2cf62a797b
JulianRDZ <rodriguezjuliann@outlook.com>
2023-06-14 03:43:48 -0300

    mail altreno

--- enviar.php
+++ enviar.php
@@ -1,5 +1,5 @@
 <?php
-    $destino= "rodriguezjuliann@outlook.com";
+    $destino= "altrenobyhummel@gmail.com";
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $edad = $_POST["edad"];
@@ -7,7 +7,7 @@
     $telefono = $_POST["telefono"];
     $correo = $_POST["correo"];
     $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre: " .$nombre . "\nApellido: " .$apellido . "\nEdad: " .$edad . "\nNacimiento" .$nacimiento . "\nTeléfono: " .$telefono . "\nCorreo: " .$correo .  "\nMensaje: " . $mensaje;
+    $contenido = "Nombre: " .$nombre . "\nApellido: " .$apellido . "\nEdad: " .$edad . "\nNacimiento" .$nacimiento . "\nTelefono: " .$telefono . "\nCorreo: " .$correo .  "\nMensaje: " . $mensaje;
     mail($destino,"PreInscripcionAltreno", $contenido);
     header("Location:gracias.html");
 
